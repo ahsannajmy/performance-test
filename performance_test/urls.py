e9b71a065524f4ce65ec9ea21f98f397fd831a66
Ahsan Najmy Ramadhan Putra Aji <ahsan@Ahsans-MacBook-Pro.local>
2023-11-23 19:12:26 +0700

    fix settings

--- performance_test/urls.py
+++ performance_test/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('forloop/', include('ForLoop.urls')),
+    path('forloop/', include('for_loop.urls')),
 ]
